614bd15f5bf450dd8a058aa73195af1819f97cdc
NovaFox161 <novaschweda@gmail.com>
2017-09-30 22:50:11 -0500

    Set up rough main activity prototype

--- app/src/main/java/com/cloudcraftgaming/universalfileuploader/UploadFile.java
+++ app/src/main/java/com/cloudcraftgaming/universalfileuploader/UploadFile.java
@@ -1,15 +1,29 @@
 package com.cloudcraftgaming.universalfileuploader;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import com.cloudcraftgaming.universalfileuploader.network.UploadManager;
+import com.kbeanie.multipicker.api.FilePicker;
+import com.kbeanie.multipicker.api.Picker;
+import com.kbeanie.multipicker.api.callbacks.FilePickerCallback;
+import com.kbeanie.multipicker.api.entity.ChosenFile;
+
+import java.util.List;
 
 public class UploadFile extends AppCompatActivity {
+    FilePicker filePicker;
+
+    Button fileSelect;
+    Button uploadButton;
+
+    Spinner selectedUploader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +32,45 @@ public class UploadFile extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        filePicker = new FilePicker(this);
+
+        // filePicker.allowMultiple();
+        // filePicker.
+        filePicker.setFilePickerCallback(new FilePickerCallback() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onFilesChosen(List<ChosenFile> files) {
+                // Display Files
+            }
+
+            @Override
+            public void onError(String message) {
+                // Handle errors
             }
         });
+
+        //Register elements
+        fileSelect = findViewById(R.id.select_file);
+        uploadButton = findViewById(R.id.upload_button);
+        selectedUploader = findViewById(R.id.selected_uploader);
+
+        //Do button things
+        fileSelect.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                filePicker.pickFile();
+            }
+        });
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (selectedUploader.getSelectedItemPosition() == 0) {
+                    //TODO: Add alert dialog for no uploader selected
+                } else {
+                    //Let the upload manager go from here.
+                    UploadManager.getManager().handleUpload(filePicker, selectedUploader);
+                }
+            }
+        });
+
     }
 
     @Override
@@ -44,9 +89,18 @@ public class UploadFile extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            //TODO: Make settings to handle Nothing Domains Auth key and other settings!!
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Picker.PICK_FILE && resultCode == RESULT_OK) {
+            filePicker.submit(data);
+            fileSelect.setText(data.getDataString());
+        }
     }
 }
---
+++ app/src/main/java/com/cloudcraftgaming/universalfileuploader/network/UploadManager.java
@@ -0,0 +1,28 @@
+package com.cloudcraftgaming.universalfileuploader.network;
+
+import android.widget.Spinner;
+import com.kbeanie.multipicker.api.FilePicker;
+
+/**
+ * Created by Nova Fox on 9/30/17.
+ * Website: www.cloudcraftgaming.com
+ * For Project: universal-file-uploader
+ */
+
+public class UploadManager {
+    private static final UploadManager instance = new UploadManager();
+
+    public static UploadManager getManager() {
+        return instance;
+    }
+
+    private UploadManager() {
+    }
+
+    public boolean handleUpload(FilePicker filePicker, Spinner selectedUploader) {
+        if (selectedUploader.getSelectedItemPosition() == 1) {
+            //TODO: Handle upload to nothing domains
+        }
+        return false;
+    }
+}
