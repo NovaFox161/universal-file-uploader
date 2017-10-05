f76bdff23cbbf61121e6e8d1402f0db7ca878305
NovaFox161 <novaschweda@gmail.com>
2017-10-04 20:20:34 -0500

    Add support for rokket.space

--- app/src/main/java/com/cloudcraftgaming/universalfileuploader/network/Host.java
+++ app/src/main/java/com/cloudcraftgaming/universalfileuploader/network/Host.java
@@ -12,7 +12,8 @@ public enum Host {
     POMF_CAT("pomf.cat", "http://pomf.cat/upload.php", "files[]", 75, HostType.POMF),
     POMFE_CO("pomfe.co", "https://pomfe.co/upload.php", "files[]", 100, HostType.POMF),
     VIDGA_ME("vidga.me", "https://vidga.me/upload.php", "files[]", 100, HostType.POMF),
-    COMFY_MOE("comfy.moe", "https://comfy.moe/upload.php", "files[]", 50, HostType.POMF);
+    COMFY_MOE("comfy.moe", "https://comfy.moe/upload.php", "files[]", 50, HostType.POMF),
+    ROKKET_SPACE("rokket.space", "https://rokket.space/upload", "files[]", 150, HostType.POMF);
 
     private String name;
     private String url;
